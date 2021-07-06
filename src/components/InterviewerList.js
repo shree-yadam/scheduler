98ee88ed3e4db0ceef691c3ff94b3f9081e605ea
Shree Devi Yadam <shree.yadam@gmail.com>
2021-07-06 14:34:54 -0400

    add InterViewList component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import InterviewerListItem from './InterviewerListItem';
+import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
 
@@ -9,8 +10,8 @@ export default function InterviewerList(props) {
       id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      selected={interviewer.selected}
-      setInterviewer={interviewer.setInterviewer}
+      selected={(props.interviewer === interviewer.id)}
+      setInterviewer={props.setInterviewer}
     />    
   );
   return (
