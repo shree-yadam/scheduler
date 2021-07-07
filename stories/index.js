5fa65fc733994a2da5a6cf31f01c2f358589eb31
Shree Devi Yadam <shree.yadam@gmail.com>
2021-07-07 00:29:28 -0400

    add component Appointment

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -153,4 +153,20 @@ storiesOf("Appointment", module)
 .add("Error Deleting", () => <Error message="Could not delete appointment." onClose={action("onClose")} />)
 .add("Form Edit", () => <Form name="Richard Burre" interviewers={interviewers} interviewer={3} onSave={action("onSave")} 
   onCancel={action("onCancel")} />)
-.add("Form Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />);
+.add("Form Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
+.add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="12pm" />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  ))
+.add("Appointment Booked", () => (
+  <Fragment>
+    <Appointment
+      id={1}
+      time="12pm"
+      interview={{ student: "Lydia Miller-Jones", interviewer }}
+    />
+    <Appointment id="last" time="1pm" />
+  </Fragment>
+));
